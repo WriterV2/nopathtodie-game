ea19014f4bf733872906bd5f79124138787c03ee
WriterV2 <primary-v2@protonmail.com>
2024-05-24 17:34:35 +0200

    Feat: Add scenario

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Scenario {
+    level: u32,
+    location: [u32; 2],
+    story: &'static str,
+    death_story: &'static str,
 }
+
+const SCENARIOS: [Scenario; 1] = [Scenario {
+    level: 1,
+    location: [0, 0],
+    story: "",
+    death_story: "",
+}];
+
+fn main() {}
